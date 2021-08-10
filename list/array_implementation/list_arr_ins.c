852773007d1bdedf8a501310b4ff2d7eaa918c57
Janaki Meena <mjanakimeena@gmail.com>
2021-08-10 21:59:58 +0530

    All insertions done

--- list/array_implementation/list_arr_ins.c
+++ list/array_implementation/list_arr_ins.c
@@ -52,19 +52,38 @@ int InsertBeg(list *l,int x)
 	l->no_of_elements++;
 	return 1;
 }
+int InsertEnd(list*l, int x)
+{
+	if(IsFull(*l))
+	return 0;
+	l->elements[l->no_of_elements++] = x;
+	return 1;
+}
+int InsertPos(list* l, int pos, int x)
+{
+	int i;
+	if((IsFull(*l))||(pos<=0)||(pos>l->no_of_elements+1))
+	return 0;
+	for(i=l->no_of_elements-1;i>=pos-1;i--)
+	{
+		l->elements[i+1] = l->elements[i];
+	}
+	l->elements[pos-1] = x;
+	l->no_of_elements++;
+	return 1;
+}
 int main()
 {
 	list l;
 	int n;
 	scanf("%d",&n);
-	l = CreateList(n);
-	
-	printf("%d\n",IsEmpty(l));
-	printf("%d\n",IsFull(l));
+	l = CreateList(n);	
 	InsertBeg(&l,3);
-	InsertBeg(&l,2);
+	InsertEnd(&l,2);
 	InsertBeg(&l,1);
-	PrintList(l);
-	if (!InsertBeg(&l,4))
+	if (!InsertPos(&l,4,10))
+	printf("Insert pos failed\n");
+	if (!InsertEnd(&l,4))
 	printf("Insertion failed\n");
+	PrintList(l);
 }
