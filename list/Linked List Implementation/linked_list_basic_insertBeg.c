cb1cac7ab6aa4745557a8ab0424d08c0fe0e3a77
Janaki Meena <mjanakimeena@gmail.com>
2021-08-18 22:52:01 +0530

    Insertend added

--- list/Linked List Implementation/linked_list_basic_insertBeg.c
+++ list/Linked List Implementation/linked_list_basic_insertBeg.c
@@ -44,11 +44,28 @@ int InsertBeg(llist* l,int data)
     l->head = new_node;
     return 1;
 }
+//Insert End 
+int InsertEnd(llist* l,int data)
+{
+    node *temp,*new_node;
+    if(IsEmpty(*l))   
+    return InsertBeg(l,data);
+    temp = l->head;
+    new_node = CreateNode(data);
+    if (new_node==NULL)
+    return 0;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next = new_node;
+    return 1;
+}
 void PrintList(llist l)
 {
     node* temp;
     temp =l.head;
-    printf("List is getting printed\n");
+    printf("List is getting printed\t");
     while(temp)
     {
         printf("%d\n",temp->data);
@@ -61,7 +78,8 @@ int main()
     printf("%d returned by isempty\n",IsEmpty(l));
     CreateList(&l);
     printf("%d returned after create\n",IsEmpty(l));
-    InsertBeg(&l,1);
-    InsertBeg(&l,2);
+    //InsertBeg(&l,1);
+    //InsertBeg(&l,2);
+    InsertEnd(&l,5);
     PrintList(l);
 }
